66bdba7d1118110d5adabb6f3f47f4abaaf7ea8c
Werner <werner_a_e@yahoo.es>
2015-04-19 19:21:41 -0430

    Response class created

--- server.cr
+++ server.cr
@@ -7,7 +7,7 @@ module Server
 
     def initialize
       @params   = {} of String => Array(String)
-      @actions  = {} of Tuple => Hash(String, Array(String)) -> String
+      @actions  = [] of Response
       @location = ""
       @method   = nil
     end
@@ -16,17 +16,18 @@ module Server
       server = HTTP::Server.new port, do |request|
         begin
           p request
-          @params = CGI.parse(request.body.to_s)
 
-          action = { @method || request.method, request.path.to_s }
+          action = @actions.find {|response| response.method == (@method || request.method) && 
+                                             response.match_path?(request.path.to_s) }
 
-          if @actions.has_key?(action)
-            if action[0] == "GET"
+          if action
+            action.params_from_request = CGI.parse(request.body.to_s)
+            if action.path == "GET"
               @method = nil
-              HTTP::Response.ok "text/html", @actions[action].call(@params)
+              HTTP::Response.ok "text/html", action.block.call(action.get_params)
             else
               @method = "GET"
-              HTTP::Response.new 307, @actions[action].call(@params), HTTP::Headers{"Location": @location}
+              HTTP::Response.new 307, action.block.call(action.get_params), HTTP::Headers{"Location": @location}
             end
           else
             HTTP::Response.not_found
@@ -40,13 +41,34 @@ module Server
 
     {% for method in %w(get post put delete patch) %}
       def {{method.id}}(route, &block : Hash(String, Array(String)) -> String)
-        @actions[{"{{method.id}}".upcase, route.to_s}] = block
+        @actions << Response.new("{{method.id}}".upcase, 
+                                 route.to_s, 
+                                 route.to_s.scan(/(:.+?(?=\/))/).map(&.[](0)), 
+                                 block)
         yield(params)
       end
     {% end %}
 
     def redirect_to(route)
       @location = route
+    end
+  end
+
+  class Response
+    property path
+    property method
+    property params
+    property block
+    setter params_from_request
+
+    def initialize(@method, @path, @params, @block)
+    end
+
+    def get_params
+    end
+
+    def match_path?(path)
+      return true
     end
   end
 end
