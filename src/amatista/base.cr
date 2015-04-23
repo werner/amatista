dab403816145d443ab5ddc28b40495e1f6dfbb8f
Werner <werner_a_e@yahoo.es>
2015-04-23 16:44:55 -0430

    making refactoring

--- src/amatista/base.cr
+++ src/amatista/base.cr
@@ -17,22 +17,17 @@ class Amatista::Base
       begin
         p request
 
-        action = Response.new.process_request(request, @actions)
+        response = Response.new(request)
+
+        action = response.process_static(request.path.to_s)
+        action = response.find_action(@actions) unless action
 
         return HTTP::Response.not_found unless action
 
-        action.request_path = request.path.to_s
-        action.add_params(CGI.parse(request.body.to_s))
-        @params = action.get_params
+        @params = response.process_params(action)
 
-        case action.method
-        when "GET"
-          HTTP::Response.ok "text/html", action.block.call(@params)
-        when "POST", "PUT", "DELETE", "PATCH"
-          HTTP::Response.new 307, action.block.call(@params), HTTP::Headers{"Location": @location}
-        else
-          raise "Path not Found"
-        end
+        response.process_request(action, @params, @location)
+
       rescue e
         HTTP::Response.error "text/plain", "Error: #{e}"
       end
--- src/amatista/response.cr
+++ src/amatista/response.cr
@@ -1,9 +1,11 @@
 class Amatista::Response
+  property request
 
-  def process_request(request, actions)
-    return if process_static(request.path.to_s)
+  def initialize(@request)
+  end
 
-    actions.find {|action_request| action_request.match_path?(request.path.to_s) }
+  def find_action(actions)
+    actions.find {|action_request| action_request.match_path?(@request.path.to_s) }
   end
 
   def process_static(path)
@@ -11,6 +13,23 @@ class Amatista::Response
       file = File.join(Dir.working_directory, path)
       Request.new("GET", path, 
                    ->(x : Hash(String, Array(String))){ File.read(file) }) if File.exists?(file)
+    end
+  end
+
+  def process_params(action)
+    action.request_path = @request.path.to_s
+    action.add_params(CGI.parse(@request.body.to_s))
+    action.get_params
+  end
+
+  def process_request(action, params, location)
+    case action.method
+    when "GET"
+      HTTP::Response.ok "text/html", action.block.call(params)
+    when "POST", "PUT", "DELETE", "PATCH"
+      HTTP::Response.new 307, action.block.call(params), HTTP::Headers{"Location": location}
+    else
+      raise "Path not Found"
     end
   end
 end
