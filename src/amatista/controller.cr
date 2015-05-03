bd3ed8eb947c6019ce4bd8fc17daca1802c5f209
Werner <werner_a_e@yahoo.es>
2015-05-03 16:47:50 -0430

    add properties to handler to process sessions

--- src/amatista/controller.cr
+++ src/amatista/controller.cr
@@ -1,8 +1,10 @@
 require "./helpers"
+require "./sessions"
 
 module Amatista
   class Controller
     extend Helpers
+    extend Sessions
 
     {% for method in %w(get post put delete patch) %}
       def self.{{method.id}}(path, &block : Hash(String, Array(String)) -> HTTP::Response)
--- src/amatista/hanlder.cr
+++ src/amatista/hanlder.cr
@@ -6,10 +6,18 @@ module Amatista
   class Handler
     property params
     property routes
+    property sessions
+    property cookie_hash
+    property secret_key
+    property request
 
     def initialize
-      @params   = {} of String => Array(String)
-      @routes  = [] of Route
+      @params      = {} of String => Array(String)
+      @routes      = [] of Route
+      @sessions    = {} of String => Hash(String, String)
+      @cookie_hash = ""
+      @secret_key  = ""
+      @request     = nil
     end
   end
 end
