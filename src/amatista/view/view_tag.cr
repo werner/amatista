f762ad6d6d796e4f8d7bdee86307e90d95955096
Werner <werner_a_e@yahoo.es>
2015-08-19 17:04:52 -0430

    add HTML.escape to text_field tag

--- src/amatista/view/view_tag.cr
+++ src/amatista/view/view_tag.cr
@@ -7,7 +7,8 @@ module Amatista
     def text_field(object_name, method, raw_options = [] of Hash(Symbol, String))
       options = options_transfomed(raw_options)
       str_result = StringIO.new
-      str_result << "<input type=\"text\" id=\"#{object_name}_#{method}\" name=\"#{object_name}[#{method}]\""
+      str_result << "<input type=\"text\" id=\"#{HTML.escape(object_name.to_s)}_#{HTML.escape(method.to_s)}\" "
+      str_result << "name=\"#{HTML.escape(object_name.to_s)}[#{HTML.escape(method.to_s)}]\""
       str_result << " #{options}" unless options.empty?
       str_result << " />"
       str_result.to_s
@@ -25,7 +26,7 @@ module Amatista
     def form_tag(url, method = "post", raw_options = [] of Hash(Symbol, String))
       options = options_transfomed(raw_options)
       str_result = StringIO.new
-      str_result << "<form action=\"#{url}\" method=\"#{method}\""
+      str_result << "<form action=\"#{HTML.escape(url)}\" method=\"#{HTML.escape(method)}\""
       str_result << " #{options}" unless options.empty?
       str_result << ">"
       str_body_result = StringIO.new
