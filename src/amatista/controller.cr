7575ff693138fb33127cf4624ec991214f8b465c
Werner <werner_a_e@yahoo.es>
2015-08-25 12:25:14 -0430

    add module to Route and Filter classes in controller calls

--- src/amatista/controller.cr
+++ src/amatista/controller.cr
@@ -10,13 +10,13 @@ module Amatista
       # Creates 5 methods to handle the http requests from the browser.
       {% for method in %w(get post put delete patch) %}
         def self.{{method.id}}(path, &block : Hash(String, Array(String)) -> HTTP::Response)
-          $amatista.routes << Route.new({{@type}}, "{{method.id}}".upcase, path.to_s, block)
+          $amatista.routes << Amatista::Route.new({{@type}}, "{{method.id}}".upcase, path.to_s, block)
           yield($amatista.params)
         end
       {% end %}
 
       def self.before_filter(paths = [] of String, &block)
-        $amatista.filters << Filter.new({{@type}}, paths, block)
+        $amatista.filters << Amatista::Filter.new({{@type}}, paths, block)
       end
     end
 
