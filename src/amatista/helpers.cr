708fba34a9f99133a01b66f047a08653e3f7aff9
Werner <werner_a_e@yahoo.es>
2015-09-03 15:50:43 -0430

    add_headers method to add headers to the response

--- src/amatista/helpers.cr
+++ src/amatista/helpers.cr
@@ -11,19 +11,28 @@ module Amatista
     # redirect_to "/tasks"
     # ```
     def redirect_to(path)
-      HTTP::Response.new 303, "redirection", HTTP::Headers{"Location": path}
+      HTTP::Response.new 303, "redirection", add_headers({location: path})
     end
 
     # Makes a respond based on context type
     # The body argument should be string if used html context type
     def respond_to(context, body)
       context = Mime.from_ext(context).to_s
-      HTTP::Response.new(200, body, process_header(context))
+      HTTP::Response.new(200, body, add_headers({context: context}))
     end
 
-    private def process_header(context)
+    def add_headers(headers = {} of Symbol => String)
       header = HTTP::Headers.new
-      header.add("Content-Type", context)
+      headers.map do |type, value|
+        case type
+        when :context
+          header.add("Content-Type", value)
+        when :location
+          header.add("Location", value)
+        when :cache
+          header.add("Cache-Control", value)
+        end
+      end
       if !$amatista.sessions.empty? && !has_session?
         header.add("Set-Cookie", send_sessions_to_cookie)
       end
